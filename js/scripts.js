8e6378da4caff10d7a5e53edc5a606367c4431fc
Michael Fritz <fritz.michael11@gmail.com>
2018-03-23 16:52:36 -0700

    adjust javascript to push the checkboxe selections into an array, not working 100%

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 // Business Logic
 function userPizza(size, toppings) {
   this.size = size;
-  this.toppings = toppings;
+  this.toppings = [];
 }
 
 //UI Logic
@@ -9,9 +9,12 @@ $(document).ready(function(){
   $("form#pizzaToppings").submit(function(event) {
       event.preventDefault();
       debugger;
-      var pizzaSize = $("select#sizeSelect").val();
-      var pizzaToppings = $("input:checkbox[name=pizzaQuestions]:checked").val();
       var newPizza = new userPizza(pizzaSize, pizzaToppings)
+      var pizzaSize = $("select#sizeSelect").val();
+      var pizzaToppings = $("input:checkbox[name=pizzaQuestions]:checked").each(function(){
+        var eachTopping = $(this).val();
+        newPizza.toppings.push(pizzaToppings);
+      });
       $("input:checkbox[name=pizzaQuestions]:checked").each(function(){
 
       })
