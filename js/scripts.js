43a8c0b20bb7e98b47c33541ef1fc77a34d08d3d
Michael Fritz <fritz.michael11@gmail.com>
2018-03-27 15:38:53 -0700

    add toppings addition on checkbox click, using opacaity css property.

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,33 @@ function userPizza(size, toppings) {
   this.toppings = [];
 }
 
+var show = 1;
+var hide = 0.0;
+var fade_speed = 'slow';
+var makePizza;
+
 //UI Logic
 $(document).ready(function(){
+  //special test for checkbox/images
+  $('input[name="pizzaQuestions"]').each(function() {
+    makePizza = "#image_" + $(this).val();
+    if($(this).prop('checked')) {
+      $(makePizza).css('opacity', show);
+    } else {
+      $(makePizza).css('opacity', hide);
+    }
+  });
+
+  $('input[name="pizzaQuestions"]').change(function(){
+    makePizza = "#image_" + $(this).val();
+    if($(this).prop('checked')) {
+      $(makePizza).fadeTo(fade_speed, show);
+    } else {
+      $(makePizza).fadeTo(fade_speed, hide);
+    }
+  });
+
+  //end test
   $("form#pizzaToppings").submit(function(event) {
       event.preventDefault();
       debugger;
